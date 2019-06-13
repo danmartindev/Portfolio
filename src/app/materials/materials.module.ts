f19c3c6582da5847005a6f9b8df0d5766b76c183
Dan Martin <danmartindev@gmail.com>
2019-06-12 22:18:43 -0400

    First Commit

---
+++ src/app/materials/materials.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+@NgModule({
+  exports:[
+    BrowserAnimationsModule,
+    MatToolbarModule
+  ]
+})
+export class MaterialsModule { }
