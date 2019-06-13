c8342443698f7a0990bba7d818aba9d7d306f20c
Dan Martin <danmartindev@gmail.com>
2019-06-12 22:20:34 -0400

    imported materials module/new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MaterialsModule } from './materials/materials.module';
+
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    MaterialsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
