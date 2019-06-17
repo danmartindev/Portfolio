88aa766039c594eb487a5bcb9aa7ef0a1a31b0bf
Dan Martin <danmartindev@gmail.com>
2019-06-17 17:19:54 -0400

    added interface for links handlers for mouseover events

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,17 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+export interface Link {
+  image: string,
+  text: string,
+  isHidden: boolean
+}
 
 @Component({
   selector: 'main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
 
-  onHover=true;
+export class MainComponent {
 
-  constructor() { }
+  // Links of main page //
+  links: Link[] = [
+    {image: "memory", text: "My Work", isHidden: true},
+    {image: "tag_faces", text: "About Me", isHidden: true},
+    {image: "insert_drive_file", text: "My Resume", isHidden: true}
+  ];
 
-  ngOnInit() {
+  mouseEnter(lText){
+    lText.isHidden = false;
+  }
+
+  mouseLeave(lText){
+    lText.isHidden = true;
   }
 
 }
