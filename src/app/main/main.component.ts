33830766eac55916e59646b88573af7cc6c6db98
Dan Martin <danmartindev@gmail.com>
2019-06-17 21:21:56 -0400

    added animations to component added state to Link iface

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,32 +1,39 @@
 import { Component } from '@angular/core';
+import { fadeUp, mainAnim } from '../animations';
 
 export interface Link {
   image: string,
   text: string,
-  isHidden: boolean
+  isHidden: boolean,
+  state: any
 }
 
 @Component({
   selector: 'main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  animations: [
+    fadeUp,
+    mainAnim
+  ]
 })
 
 export class MainComponent {
 
   // Links of main page //
   links: Link[] = [
-    {image: "memory", text: "My Work", isHidden: true},
-    {image: "tag_faces", text: "About Me", isHidden: true},
-    {image: "insert_drive_file", text: "My Resume", isHidden: true}
+    {image: "memory", text: "My Work", isHidden: true, state: '*'},
+    {image: "tag_faces", text: "About Me", isHidden: true, state: '*'},
+    {image: "insert_drive_file", text: "My Resume", isHidden: true, state: '*'}
   ];
 
-  mouseEnter(lText){
-    lText.isHidden = false;
+  mouseEnter(l){
+    l.isHidden = false;
+    l.state = 'hovered';
   }
 
-  mouseLeave(lText){
-    lText.isHidden = true;
+  mouseLeave(l){
+    l.isHidden = true;
+    l.state = '*';
   }
-
 }
