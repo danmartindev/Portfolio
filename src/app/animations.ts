2cda158f8bfe0bde7c8bd7dce0639dc72e50bc91
Dan Martin <danmartindev@gmail.com>
2019-06-17 21:20:46 -0400

    added animations for main icons

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,1 +1,24 @@
-import { trigger, state, transition, style, animate, query, stagger } from '@angular/animations';
+import { trigger, state, transition, style, animate, query, stagger, animateChild } from '@angular/animations';
+
+export let fadeUp =  
+  trigger('fadeUp', [
+
+    state('void', style({ opacity: 0, transform: 'translateY(10px)'})),
+    state('hovered', style({  transform: 'translateY(-10px)', color: '#FFDF56'})),
+
+    transition('* <=> hovered', [
+        animate('250ms ease-in-out'),
+    ]),
+
+    transition(':enter', [
+        animate('300ms ease-in')
+    ])
+  ])
+
+export let mainAnim =
+  trigger('mainAnim', [
+    state('hovered', style({ backgroundColor: 'red'})),
+    transition(':enter', [
+        query('@fadeUp', stagger(200, animateChild()))
+    ]),
+  ])
