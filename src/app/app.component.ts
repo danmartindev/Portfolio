150298c7c010db52d5edfc90c7086c6aee9fa6b7
Dan Martin <danmartindev@gmail.com>
2019-06-18 21:10:32 -0400

    updated for router animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { routingAnims } from './animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations:[
+    routingAnims
+  ]
 })
 export class AppComponent {
   title = 'Portfolio';
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
 }
