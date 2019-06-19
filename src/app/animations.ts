0e22fd552239ba6817cfd1ace66fc35325b25be9
Dan Martin <danmartindev@gmail.com>
2019-06-18 21:09:45 -0400

    began animations for router-outlet

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,4 +1,4 @@
-import { trigger, state, transition, style, animate, query, stagger, animateChild } from '@angular/animations';
+import { trigger, state, transition, style, animate, query, stagger, animateChild, group } from '@angular/animations';
 
 export let fadeUp =  
   trigger('fadeUp', [
@@ -11,14 +11,62 @@ export let fadeUp =
     ]),
 
     transition(':enter', [
-        animate('300ms ease-in')
+        animate('300ms 250ms ease-in')
     ])
+  ])
+
+export let slideOut =
+  trigger('slideOut', [
+
+    state('leaving', style({  transform: 'translateX(-30px)'})),
+
+    transition('* <=> hovered', [
+      animate('1s ease-in-out', style({ transform: 'translateX(-30px)'})),
+    ]),
   ])
 
 export let mainAnim =
   trigger('mainAnim', [
-    state('hovered', style({ backgroundColor: 'red'})),
+
     transition(':enter', [
         query('@fadeUp', stagger(200, animateChild()))
     ]),
+
+    transition(':leave',[
+      query(':self', animate('1s ease-in-out', style({ transform: 'translateX(-30px)'})))
+    ])
+  ])
+
+  export const routingAnims = trigger('routeAnimations', [
+    // transition('* <=> *', [
+    //   /* order */
+    //   /* 1 */ query(':enter, :leave', style({ position: 'fixed', width:'100%', height: '80%' })
+    //     , { optional: true }),
+    //   /* 2 */ group([  // block executes in parallel
+    //     query(':enter', [
+    //       style({ transform: 'translateX(100%)' }),
+    //       animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))
+    //     ], { optional: true }),
+    //     query(':leave', [
+    //       style({ transform: 'translateX(0%)' }),
+    //       animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' }))
+    //     ], { optional: true }),
+    //   ])
+    // ])
+
+    transition('Main => Work', [
+      /* order */
+      /* 1 */ query(':enter, :leave', style({ position: 'fixed', width:'100%', height: '80%' })
+        , { optional: true }),
+      /* 2 */ group([  // block executes in parallel
+        query(':enter', [
+          style({ transform: 'translateX(100%)' }),
+          animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))
+        ], { optional: true }),
+        query(':leave', [
+          style({ transform: 'translateX(0%)' }),
+          animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' }))
+        ], { optional: true }),
+      ])
+    ])
   ])
