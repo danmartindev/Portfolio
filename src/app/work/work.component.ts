dfbff1c673b4f952146018b12b64b47b8bbc1c44
Dan Martin <danmartindev@gmail.com>
2019-06-19 22:39:07 -0400

    added animations

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { dropDown, mainAnim } from '../animations';
 
 @Component({
   selector: 'app-work',
   templateUrl: './work.component.html',
-  styleUrls: ['./work.component.scss']
+  styleUrls: ['./work.component.scss'],
+  animations: [
+    dropDown,
+    mainAnim
+  ]
 })
 export class WorkComponent implements OnInit {
 
