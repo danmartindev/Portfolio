45937119402503090985ebadd7eb83a33d7bf5b1
Dan Martin <danmartindev@gmail.com>
2019-06-20 13:07:15 -0400

    imported mat-tabs module

--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @NgModule({
   exports:[
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatTabsModule,
+    MatToolbarModule,
   ]
 })
 export class MaterialsModule { }
