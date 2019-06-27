dc9139a48118fa9eeb7fc31fc8bd6d0ab1ac279c
Dan Martin <danmartindev@gmail.com>
2019-06-26 21:11:07 -0400

    added collapse function and animation

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { dropDown, mainAnim } from '../animations';
+import { dropDown, mainAnim, collapser } from '../animations';
 
 @Component({
   selector: 'app-work',
   templateUrl: './work.component.html',
   styleUrls: ['./work.component.scss'],
   animations: [
+    collapser,
     dropDown,
     mainAnim
   ]
 })
 export class WorkComponent implements OnInit {
 
+  cState="*"
   constructor() { }
 
   ngOnInit() {
   }
 
+  collapse(){
+    console.log("clicked");
+    if(this.cState == "*"){
+      this.cState = "expanded";
+    } else {
+      this.cState = "*";
+    }
+  }
+  
 }
