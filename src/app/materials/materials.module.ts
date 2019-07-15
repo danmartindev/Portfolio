370367ee013ef3efa8155193d4946555526dff80
Dan Martin <danmartindev@gmail.com>
2019-07-14 20:53:17 -0400

    added chips module

--- src/app/materials/materials.module.ts
+++ src/app/materials/materials.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
@@ -13,6 +14,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   exports:[
     BrowserAnimationsModule,
     MatButtonModule,
+    MatChipsModule,
     MatExpansionModule,
     MatIconModule,
     MatMenuModule,
