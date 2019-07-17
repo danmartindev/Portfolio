02c8970b666f070ff1e5bb0ad99bda6bdd2abeea
Dan Martin <danmartindev@gmail.com>
2019-07-16 21:33:05 -0400

    spacer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 
 import { MaterialsModule } from './materials/materials.module';
 
-
 import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
