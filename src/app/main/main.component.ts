83cb561024e245355641319920414613d32b65d3
Dan Martin <danmartindev@gmail.com>
2019-07-18 23:22:53 -0400

    adjusted icon to code

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -24,7 +24,7 @@ export class MainComponent {
 
   // Links of main page //
   links: Link[] = [
-    {route: "/work", image: "memory", text: "My Work", isHidden: true, state: '*'},
+    {route: "/work", image: "code", text: "My Work", isHidden: true, state: '*'},
     {route: "/", image: "tag_faces", text: "About Me", isHidden: true, state: '*'},
     {route: "/resume", image: "insert_drive_file", text: "Resume+Skills", isHidden: true, state: '*'}
   ];
