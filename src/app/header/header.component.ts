acccfc569cd4217cd3ece5914f32bbbad6538d33
Dan Martin <danmartindev@gmail.com>
2019-07-22 22:01:18 -0400

    added state/code for button animation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,24 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { homeSlide } from '../animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations:[
+    homeSlide
+  ]
 })
 export class HeaderComponent implements OnInit {
+
+  btnState: String = '*';
 
   constructor(private myRouter: Router) { 
     this.myRouter.events.subscribe(event => {
       if(event instanceof NavigationEnd) {
-        console.log(event.url);
+        if(event.url == "/main"){
+          console.log("meep");
+          this.btnState = '*'
+        } else {
+          console.log(event.url);
+          this.btnState = 'show'
+        }
       }
     });
   }
 
   ngOnInit() {
   }
-
-  
-
 }
