334efc76a1c418ce0df31901b83d01be972eed77
Dan Martin <danmartindev@gmail.com>
2019-07-23 23:33:04 -0400

    started transitioning to real information

--- src/app/my-entries.ts
+++ src/app/my-entries.ts
@@ -1,41 +1,30 @@
 import { Entry } from './entry';
 
 export const expEntries : Entry[] = [
-    
-    {title: "iOS Developer 1", company: "Some Company", date: "june - july", 
-    content: `1111111This co-op had me working as an iOS developer on an NIH Grant project between RIT and the University of Rochester. 
+    //AHFC//
+    {title: "Freelance Wordpress Developer", company: "African Heritage Food Co-op", date: "July 2018 - December 2019", 
+    link: 'https://myahfc.com/community/', linkText: 'Visit the Site',
+    content: `This freelance project had me working with designer/developer to develop a new website for a Buffalo based non-profit. 
+    A majority of my time was spent working on the community section of the site. This implements a couple of plugins which we 
+    lightly customized for user accounts, and also some custom PHP to allow for user submitted posts and comments.`, state:"*"},
+
+    //CAMAFib
+    {title: "iOS Developer - Co-op/Internship", company: "RIT/NIH", date: "September 2017 - December 2017", 
+    content: `This co-op had me working as an iOS developer on an NIH Grant project between RIT and the University of Rochester. 
     The goal of the project was to detect atrial fibrillation, irregular heart rhythms, through a video feed. 
     <br>
     <br>
     I worked as the iOS developer on a team including myself, a data analyst, and an android developer. 
     The end product of my time at this position was an app that on the front-end had a number of built in browser “apps” that 
     the user could browse, while in the background, videos were periodically recorded through the front facing camera. 
-    These videos would have the RGB data of each frame extracted, the data sent to a firebase database, 
-    and then the video would be deleted for the user's privacy.`, state:"*"},
+    These videos would have the RGB data of each frame extracted, the data sent to a firebase database, and then the video would be deleted for the user's privacy.`, state:"*"},
 
-
-    {title: "iOS Developer 2", company: "Some Company", date: "june - july", 
-    content: `2222222This co-op had me working as an iOS developer on an NIH Grant project between RIT and the University of Rochester. 
-    The goal of the project was to detect atrial fibrillation, irregular heart rhythms, through a video feed. 
-    <br>
-    <br>
-    I worked as the iOS developer on a team including myself, a data analyst, and an android developer. 
-    The end product of my time at this position was an app that on the front-end had a number of built in browser “apps” that 
-    the user could browse, while in the background, videos were periodically recorded through the front facing camera. 
-    These videos would have the RGB data of each frame extracted, the data sent to a firebase database, 
-    and then the video would be deleted for the user's privacy.`, state:"*"},
-
-
-    {title: "iOS Developer 3", company: "Some Company", date: "june - july", 
-    content: `333333This co-op had me working as an iOS developer on an NIH Grant project between RIT and the University of Rochester. 
-    The goal of the project was to detect atrial fibrillation, irregular heart rhythms, through a video feed. 
-    <br>
-    <br>
-    I worked as the iOS developer on a team including myself, a data analyst, and an android developer. 
-    The end product of my time at this position was an app that on the front-end had a number of built in browser “apps” that 
-    the user could browse, while in the background, videos were periodically recorded through the front facing camera. 
-    These videos would have the RGB data of each frame extracted, the data sent to a firebase database, 
-    and then the video would be deleted for the user's privacy.`, state:"*"},
+    //PHT
+    {title: "Wordpress Developer - Co-op/Internship", company: "Personalized Healthcare Technology", date: "May 2017 - August 2017", 
+    content: `For this co-op I worked as a general web developer for the members of PHT, a group tied to the research and application 
+    of healthcare technologies. Most of my work involved the development of Wordpress sites and plugins. 
+    I was also responsible for some of the design work for the sites and instructing the members of PHT on how to update their 
+    sites for themselves.`, state:"*"},
 
 
     {title: "iOS Developer 3", company: "Some Company", date: "june - july", 
@@ -53,7 +42,7 @@ export const expEntries : Entry[] = [
 
 export const projEntries : Entry[] = [
     
-    {title: "iOS Developer 1", company: "Some Company", date: "june - july", 
+    {title: "Social Media Stopwatch", company: "Some Company", date: "june - july", 
     content: `1111111This co-op had me working as an iOS developer on an NIH Grant project between RIT and the University of Rochester. 
     The goal of the project was to detect atrial fibrillation, irregular heart rhythms, through a video feed. 
     <br>
@@ -75,29 +64,4 @@ export const projEntries : Entry[] = [
     the user could browse, while in the background, videos were periodically recorded through the front facing camera. 
     These videos would have the RGB data of each frame extracted, the data sent to a firebase database, 
     and then the video would be deleted for the user's privacy.`, state:"*"},
-
-
-    {title: "iOS Developer 3", company: "Some Company", date: "june - july", 
-    content: `333333This co-op had me working as an iOS developer on an NIH Grant project between RIT and the University of Rochester. 
-    The goal of the project was to detect atrial fibrillation, irregular heart rhythms, through a video feed. 
-    <br>
-    <br>
-    I worked as the iOS developer on a team including myself, a data analyst, and an android developer. 
-    The end product of my time at this position was an app that on the front-end had a number of built in browser “apps” that 
-    the user could browse, while in the background, videos were periodically recorded through the front facing camera. 
-    These videos would have the RGB data of each frame extracted, the data sent to a firebase database, 
-    and then the video would be deleted for the user's privacy.`, state:"*"},
-
-
-    {title: "iOS Developer 3", company: "Some Company", date: "june - july", 
-    content: `333333This co-op had me working as an iOS developer on an NIH Grant project between RIT and the University of Rochester. 
-    The goal of the project was to detect atrial fibrillation, irregular heart rhythms, through a video feed. 
-    <br>
-    <br>
-    I worked as the iOS developer on a team including myself, a data analyst, and an android developer. 
-    The end product of my time at this position was an app that on the front-end had a number of built in browser “apps” that 
-    the user could browse, while in the background, videos were periodically recorded through the front facing camera. 
-    These videos would have the RGB data of each frame extracted, the data sent to a firebase database, 
-    and then the video would be deleted for the user's privacy.`, state:"*"},
-
 ]
