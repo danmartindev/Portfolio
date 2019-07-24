09d90652527a387bf6c8a15481f46bd84152cde2
Dan Martin <danmartindev@gmail.com>
2019-07-23 23:33:40 -0400

    added mainanim animations

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,13 @@
+import { mainAnim } from './../animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  animations:[
+    mainAnim
+  ]
 })
 export class AboutComponent implements OnInit {
 
