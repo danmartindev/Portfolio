c5ff583e513ce537d99de6f83f67cc561240026f
Dan Martin <danmartindev@gmail.com>
2019-07-24 22:06:42 -0400

    added chips and secondary class

--- src/app/entry.ts
+++ src/app/entry.ts
@@ -5,6 +5,20 @@ export interface Entry{
     link?: String,
     linkText?: String,
     content: String,
+    chips: String[],
+    state: String,
+}
+
+export interface ProjEntry{
+    title: String,
+    type: String,
+    date: String,
+    link?: String,
+    linkText?: String,
+    link2?: String,
+    linkText2?: String,
+    content: String,
+    chips: String[],
     state: String,
 }
   
