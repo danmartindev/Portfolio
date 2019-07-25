8e870df13c0941c2b700473f4c79c35a9b9640b3
Dan Martin <danmartindev@gmail.com>
2019-07-24 22:06:54 -0400

    added chip data

--- src/app/my-entries.ts
+++ src/app/my-entries.ts
@@ -1,4 +1,4 @@
-import { Entry } from './entry';
+import { Entry, ProjEntry } from './entry';
 
 export const expEntries : Entry[] = [
     //AHFC//
@@ -6,7 +6,8 @@ export const expEntries : Entry[] = [
     link: 'https://myahfc.com/community/', linkText: 'Visit the Site',
     content: `This freelance project had me working with designer/developer to develop a new website for a Buffalo based non-profit. 
     A majority of my time was spent working on the community section of the site. This implements a couple of plugins which we 
-    lightly customized for user accounts, and also some custom PHP to allow for user submitted posts and comments.`, state:"*"},
+    lightly customized for user accounts, and also some custom PHP to allow for user submitted posts and comments.`, 
+    chips: ['Wordpress', 'PHP', 'HTML', 'CSS', 'JavaScript'], state:"*"},
 
     //CAMAFib
     {title: "iOS Developer - Co-op/Internship", company: "RIT/NIH", date: "September 2017 - December 2017", 
@@ -17,51 +18,41 @@ export const expEntries : Entry[] = [
     I worked as the iOS developer on a team including myself, a data analyst, and an android developer. 
     The end product of my time at this position was an app that on the front-end had a number of built in browser “apps” that 
     the user could browse, while in the background, videos were periodically recorded through the front facing camera. 
-    These videos would have the RGB data of each frame extracted, the data sent to a firebase database, and then the video would be deleted for the user's privacy.`, state:"*"},
+    These videos would have the RGB data of each frame extracted, the data sent to a firebase database, and then the video would be deleted for the user's privacy.`, 
+    chips: ['iOS', 'XCode', 'Mobile'], state:"*"},
 
     //PHT
     {title: "Wordpress Developer - Co-op/Internship", company: "Personalized Healthcare Technology", date: "May 2017 - August 2017", 
     content: `For this co-op I worked as a general web developer for the members of PHT, a group tied to the research and application 
     of healthcare technologies. Most of my work involved the development of Wordpress sites and plugins. 
     I was also responsible for some of the design work for the sites and instructing the members of PHT on how to update their 
-    sites for themselves.`, state:"*"},
+    sites for themselves.`, 
+    chips: ['Wordpress', 'PHP', 'HTML', 'CSS'], state:"*"},
 
-
-    {title: "iOS Developer 3", company: "Some Company", date: "june - july", 
-    content: `333333This co-op had me working as an iOS developer on an NIH Grant project between RIT and the University of Rochester. 
-    The goal of the project was to detect atrial fibrillation, irregular heart rhythms, through a video feed. 
-    <br>
-    <br>
-    I worked as the iOS developer on a team including myself, a data analyst, and an android developer. 
-    The end product of my time at this position was an app that on the front-end had a number of built in browser “apps” that 
-    the user could browse, while in the background, videos were periodically recorded through the front facing camera. 
-    These videos would have the RGB data of each frame extracted, the data sent to a firebase database, 
-    and then the video would be deleted for the user's privacy.`, state:"*"},
+    //NDA
+    {title: "Freelance Angular Developer", company: "Undisclosed", date: "Febuary 2019 - May 2019", 
+    content: `This project had me developing a social media type Angular2+ app. This project is currently under NDA.`, 
+    chips: ['Angular2+', 'HTML', 'CSS', 'SASS', 'TypeScript'], state:"*"},
 
 ]
 
-export const projEntries : Entry[] = [
+export const projEntries : ProjEntry[] = [
     
-    {title: "Social Media Stopwatch", company: "Some Company", date: "june - july", 
-    content: `1111111This co-op had me working as an iOS developer on an NIH Grant project between RIT and the University of Rochester. 
-    The goal of the project was to detect atrial fibrillation, irregular heart rhythms, through a video feed. 
+    //Social Stopwatch
+    {title: "Social Media Stopwatch", type: "Google Chrome Extension", date: " March 2019 - April 2019", 
+    link: "https://github.com/danmartindev/SocialStopwatch", linkText: "The code", 
+    link2: "https://chrome.google.com/webstore/detail/social-media-stopwatch/fahoaamoeaejjidplpjhgmaggphgnaig", linkText2: "Try it!",
+    content: `This is a Google Chrome extension that I developed for a friend. It utilizes the Chrome alarms, 
+    tabs, and local storage APIs to set timers(alarms) based on your open tabs, to help you better manage your 
+    time usage of social media and other distracting sites. 
     <br>
     <br>
-    I worked as the iOS developer on a team including myself, a data analyst, and an android developer. 
-    The end product of my time at this position was an app that on the front-end had a number of built in browser “apps” that 
-    the user could browse, while in the background, videos were periodically recorded through the front facing camera. 
-    These videos would have the RGB data of each frame extracted, the data sent to a firebase database, 
-    and then the video would be deleted for the user's privacy.`, state:"*"},
+    Timers can be set manually with the user choosing the tab by URL and setting the how long they want to
+     spend on the site. Timers can also be set automatically based on the URL. These URLs can be set by the 
+    user in the options page.
+    <br>
+    <br>
+    You can check out the code or download it to try for yourself in the links down below.`, 
+    chips: ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Chrome APIs'], state:"*"},
 
-
-    {title: "iOS Developer 2", company: "Some Company", date: "june - july", 
-    content: `2222222This co-op had me working as an iOS developer on an NIH Grant project between RIT and the University of Rochester. 
-    The goal of the project was to detect atrial fibrillation, irregular heart rhythms, through a video feed. 
-    <br>
-    <br>
-    I worked as the iOS developer on a team including myself, a data analyst, and an android developer. 
-    The end product of my time at this position was an app that on the front-end had a number of built in browser “apps” that 
-    the user could browse, while in the background, videos were periodically recorded through the front facing camera. 
-    These videos would have the RGB data of each frame extracted, the data sent to a firebase database, 
-    and then the video would be deleted for the user's privacy.`, state:"*"},
 ]
